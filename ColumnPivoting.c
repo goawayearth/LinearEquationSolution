1d1d172f7a939fd14640e10263f84efc5375a36f
TMML <tml3200035928@outlook.com>
2024-05-20 01:47:15 +0800

    correct bug

--- ColumnPivoting.c
+++ ColumnPivoting.c
@@ -9,7 +9,7 @@ void nextCalculate(int N,double** HEAD,int col){
     // 换完主元之后，需要将主元下面的数据变为0
     #pragma omp parallel for shared(HEAD, N, col) default(none)
     for(int row = col+1;row<N;row++){
-        if((*(*(HEAD+col)+col)) < 0.000001) continue;
+        if(abs_t(*(*(HEAD+col)+col)) < 0.000001) continue;
         double k = (*(*(HEAD+row)+col)) / (*(*(HEAD+col)+col));
         for(int col1 = 0;col1<N+1;col1++)
             *(*(HEAD+row)+col1) -= k * (*(*(HEAD+col)+col1)); 
